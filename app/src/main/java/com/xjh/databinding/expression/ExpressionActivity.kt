a28864a2b462b009381c09e1005a7abd1bd95c6a
ginshin <254157101@qq.com>
2020-02-15 16:14:38 +0800

    DataBinding 表达式链

--- app/src/main/java/com/xjh/databinding/expression/ExpressionActivity.kt
+++ app/src/main/java/com/xjh/databinding/expression/ExpressionActivity.kt
@@ -6,15 +6,17 @@ import android.os.Bundle
 import com.xjh.databinding.Employee
 import com.xjh.databinding.R
 import com.xjh.databinding.databinding.ActivityExpressionBinding
+import java.util.*
 
 class ExpressionActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityExpressionBinding
+    private val mRandom = Random(System.currentTimeMillis())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_expression)
-        val employee = Employee("X", "JH", false)
+        val employee = Employee("X", "JH", mRandom.nextBoolean())
         employee.avatar = "https://avatar.csdnimg.cn/D/4/D/1_xjh_shin.jpg"
         binding.employee = employee
     }
